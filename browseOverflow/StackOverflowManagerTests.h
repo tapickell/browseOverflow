8ea00093c6c140c7cdde5be1a2e1f0d72ec1a388
myappleguy <myappleguy@gmail.com>
2013-02-05 17:50:24 -0600

    added stack overflow manger tests

---
+++ browseOverflow/StackOverflowManagerTests.h
@@ -0,0 +1,13 @@
+//
+//  StackOverflowManagerTests.h
+//  browseOverflow
+//
+//  Created by Todd Pickell on 2/5/13.
+//  Copyright (c) 2013 Todd Pickell. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StackOverflowManagerTests : SenTestCase
+
+@end
---
+++ browseOverflow/StackOverflowManagerTests.m
@@ -0,0 +1,13 @@
+//
+//  StackOverflowManagerTests.m
+//  browseOverflow
+//
+//  Created by Todd Pickell on 2/5/13.
+//  Copyright (c) 2013 Todd Pickell. All rights reserved.
+//
+
+#import "StackOverflowManagerTests.h"
+
+@implementation StackOverflowManagerTests
+
+@end
